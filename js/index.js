b6e8ceb2f585d00a102cecfea85b85b0e465e9d5
Mya Rios <myarios23@gmail.com>
2022-03-07 23:49:52 -0500

    adding starter code as base

---
+++ js/index.js
@@ -0,0 +1,99 @@
+/* global - associative array to represent menu items & respective prices */
+const menu = [];
+menu["hotdogs"] = 4;
+menu["fries"] = 3.50;
+menu["soda"] = 1.50;
+menu["sauerkraut"] = 1;
+
+/* global - place order btn */
+const placeOrderBtn = document.getElementById("begin-order-btn");
+/* global - submit order btn */
+const submitOrderBtn = document.getElementById("submit-order-btn");
+/* global - modify new order btn */
+const modifyOrderBtn = document.getElementById("modify-order-btn");
+
+/* global - total order element */
+const calculatedTotal = document.getElementById("calculated-amount");
+/* global - final order element*/
+const finalOrder = document.getElementById("ordered-items");
+
+/* global - final order array */
+var order = [];
+/* global - total order amount */
+var total = 0;
+
+window.onload = () => {
+    placeOrderBtn.addEventListener("click", () =>{
+        // hide welcome page content
+       updateElementStyle("welcome-page", "none");
+
+        // show order page content
+        updateElementStyle("order-content", "block");
+    });
+
+    submitOrderBtn.addEventListener("click", ()=>{
+        calculateTotal();
+        displayReceipt();
+    });
+
+    modifyOrderBtn.addEventListener("click", ()=>{
+        // clearing total, order, and final order array
+        total = 0;
+        order = [];
+        finalOrder.innerHTML = "";
+
+        // hide welcome page content        
+        updateElementStyle("welcome-page", "none");
+      
+        // show order page content
+        updateElementStyle("order-content", "block")
+
+        // hide reciept
+        updateElementStyle("receipt", "none");
+    });
+}
+
+// Function to calculate final order
+function calculateTotal(){
+    for(item in menu){
+        let amount = parseFloat(document.getElementById(item).value);
+
+        if(!isNaN(amount)){
+            // calculate price & add to total
+            let price = menu[item];
+            let subTotal = amount * price;
+            total += subTotal;
+
+            // add item & amount to final order
+            order[item] = amount;
+        }
+    }
+}
+
+// Function to display receipt
+function displayReceipt(){
+    // hide order form
+    updateElementStyle("order-content", "none");
+    
+    // display receipt
+    updateElementStyle("receipt", "inline");
+
+    let dollarUS = Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+    });
+
+    calculatedTotal.innerHTML = dollarUS.format(total);
+
+    for(food in order){
+        let foodDiv = document.createElement("div");
+        foodDiv.innerHTML = food + " x " + order[food];
+        finalOrder.appendChild(foodDiv);
+    }
+}
+
+// Function to show/hide specific page based on element ID
+function updateElementStyle(elementID, styleType){
+    let elementDiv = document.getElementById(elementID);
+    elementDiv.style.display = styleType;
+}
